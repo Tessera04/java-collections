59a0fc4658478019a496481eedb0f7daf3991b19
Matias <matute_11@hotmail.com.ar>
2023-02-06 00:12:22 -0300

    Creando listas con objetos

---
+++ src/ar/com/alura/Clase4.java
@@ -0,0 +1,23 @@
+package ar.com.alura;
+
+import java.util.ArrayList;
+
+public class Clase4 {
+	public static void main(String[] args) {
+		
+		Curso Curso1 = new Curso("Geometria", 30);
+		Curso Curso2 = new Curso("Fisica", 20);
+		Curso Curso3 = new Curso("Geografia", 10);
+		Curso Curso4 = new Curso("Lengua", 40);
+		Curso Curso5 = new Curso("Matematica", 50);
+		
+		ArrayList<Curso> cursos = new ArrayList<>();
+		cursos.add(Curso1);
+		cursos.add(Curso2);
+		cursos.add(Curso3);
+		cursos.add(Curso4);
+		cursos.add(Curso5);
+		
+		System.out.println(cursos);
+	}
+}
